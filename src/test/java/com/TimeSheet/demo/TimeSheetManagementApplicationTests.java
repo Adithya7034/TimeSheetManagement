2deed62d4d214db3dfd13e7189bd0ddd0beea748
unknown <adithyaranjith2000@gmail.com>
2022-02-03 23:21:28 +0530

    Initial commit

---
+++ src/test/java/com/TimeSheet/demo/TimeSheetManagementApplicationTests.java
@@ -0,0 +1,29 @@
+package com.TimeSheet.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@SpringBootTest
+class TimeSheetManagementApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
+
+
+	
